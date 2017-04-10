826f0b61210f3074f5347435b453b7494afba83f
Jacob Schein <jacobschein0429@gmail.com>
2017-04-09 19:23:55 -0500

    Update FileParser.h

--- lab3/FileParser.h
+++ lab3/FileParser.h
@@ -10,10 +10,11 @@ using namespace std;
 enum index { programName, gameName, numArgs };
 
 enum result {
-	success = 0, failToOpen = -1, wrongNumArgs = -2, failToReadLine = -3, failToGrabDimensions = -4, failToGrabPieces = -5, tooManyPieces = -6, quitEntered = -7, stalemateErr = -8, wrongGameName = -9, invalidGamePiece = -10, nonNumberInput = -11, invalidNum = -12
+	success = 0, failToOpen = -1, wrongNumArgs = -2, failToReadLine = -3, failToGrabDimensions = -4, failToGrabPieces = -5,
+	tooManyPieces = -6, quitEntered = -7, stalemateErr = -8, wrongGameName = -9, invalidGamePiece = -10, nonNumberInput = -11,
+	invalidNum = -12, nullPtr = -13, pointerAlreadyInit = -14, noMemory = -15
 };
 
-int fileParser(vector<string> &, char *);
 
 int usageMessage(const string &, const string &, int);
 
