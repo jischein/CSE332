67312a8506924129f9f97d5c6cddd2f08fb69b71
Jacob Schein <jacobschein0429@gmail.com>
2017-04-09 19:30:42 -0500

    Update lab3.cpp

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -10,19 +10,42 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	try {
-		GameBase* game = GameBase::start(argv, argc);
-		if (game != 0) {
-			return game->play();
-		}
-		else {
-			cout << "NOT RIGHT PTR" << endl;
-			cout << "U SENT ZEORORORO" << endl;
-		}
+
+		GameBase::start(argv, argc);
+		return GameBase::instance()->play();
+
 	}
+
 	catch (result r) {
-		cout << r;
-		cout << "caught some sort of rerrorrrr" << endl;
+		if (r == wrongNumArgs) {
+			return usageMessage("Lab3", " You have supplied the wrong Number of Arguments\nInput must be as follows: '<input-file> GameName' ", wrongNumArgs);
+		}
+		else if (r == wrongGameName) {
+			return usageMessage("Lab3", " Input must be as follows: '<input-file> GameName' You have supplied an incorrect GameName\nCurrently available games: 'NineAlmonds, MagicSquare'\n", wrongGameName);
+		}
+		else if (r == invalidGamePiece) {
+			return usageMessage("Lab3", " Invalid GamePiece found. Try again ", invalidGamePiece);
+		}
+		else if (r == tooManyPieces) {
+			return usageMessage("Lab3", " Too many GamePieces given board dimensions. ERR ", tooManyPieces);
+		}
+		else if (r == nonNumberInput) {
+			return usageMessage("Lab3", " NonNumeric input submitted when Numeric input required. ERR ", tooManyPieces);
+		}
+		else if (r == invalidNum) {
+			return usageMessage("Lab3", " Value less than or equal to 0 inputted when positive value required. ERR ", tooManyPieces);
+		}
+		else if (r == pointerAlreadyInit) {
+			return usageMessage("Lab3", " GameBase pointer already initialized. ", pointerAlreadyInit);
+
+		}
 	}
+
+	catch (bad_alloc b)
+	{
+		return usageMessage("Lab3", "Memory could not be allocated.", noMemory);
+	}
+
     return 0;
 }
 
