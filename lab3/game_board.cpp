a1735c0c2368f6307edb8d1e9a7bedb500a0d7dd
Jacob Schein <jacobschein0429@gmail.com>
2017-04-09 19:26:30 -0500

    Update game_board.cpp

--- lab3/game_board.cpp
+++ lab3/game_board.cpp
@@ -11,127 +11,7 @@
 
 using namespace std;
 //function that reads board game dimensions
-int read_dimension(ifstream & file, unsigned & dim_x, unsigned & dim_y) {
 
-	string line;
-	//counter to tell how many extractions were successfull 
-	int num_success = 0;
-
-	if (file.is_open()) {
-
-		if (getline(file, line)) {
-
-			istringstream iss(line);
-
-			if (iss >> dim_x) {
-				num_success++;
-			}
-
-			if (iss >> dim_y) {
-				num_success++;
-			}
-
-		}
-		else {
-			cout << "FAILED TO READ LINE" << endl;
-			cerr << failToReadLine << endl;
-			return failToReadLine;
-		}
-
-	}
-
-	else {
-		cout << "FAILED TO OPEN FILE" << endl;
-		cerr << failToOpen << endl;
-		return failToOpen;
-	}
-	//if both board dimensions were extracted, success
-	if (num_success == 2) {
-		return success;
-	}
-
-	else if (num_success == 1) {
-		cout << "FAILED TO EXTRACT BOTH DIMENSIONS" << endl;
-		cerr << failToGrabDimensions << endl;
-		return failToGrabDimensions;
-	}
-
-	else {
-		cout << "LINE WAS READ BUT FAILED TO EXTRACT DIMENSIONS" << endl;
-		cerr << failToGrabDimensions << endl;
-		return failToGrabDimensions;
-	}
-
-}
-
-int read_board(ifstream & file, vector<game_piece> & pieces, unsigned dim_x, unsigned dim_y) {
-	string line;
-	//counter to tell how many extractions were successfull 
-	int successful_pieces = 0;
-
-	if (file.is_open()) {
-
-		while (getline(file, line)) {
-			string name;
-			string representation;
-			string color;
-			unsigned pos_x = 0;
-			unsigned pos_y = 0;
-
-			istringstream iss(line);
-
-			if (iss >> color) {
-
-				if (iss >> name) {
-
-					if (iss >> representation) {
-
-						if (iss >> pos_x) {
-
-							if (iss >> pos_y) {
-
-								piece_color color_transform = string_to_pieceColor(color);
-
-								if (pos_x < dim_x) {
-
-									if (pos_y < dim_y) {
-
-										int index = (dim_x)* pos_y + pos_x;
-
-										game_piece piece = game_piece(color_transform, name, representation);
-
-										pieces[index] = piece;
-
-										successful_pieces++;
-
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	else {
-		cout << "FAILED TO OPEN FILE" << endl;
-		cerr << failToOpen << endl;
-		return failToOpen;
-	}
-
-	//if at least one piece was read successfully, success
-	if (successful_pieces >= 1) {
-		return success;
-	}
-
-	else {
-		cout << "LINE WAS READ BUT NO PIECES WERE GRABBED SUCCESSFULLY" << endl;
-		cerr << failToGrabPieces << endl;
-		return failToGrabPieces;
-	}
-
-}
 
 ostream& print_board(ostream &o, int spacing, const vector<game_piece> & board, unsigned int dim_x, unsigned int dim_y) {
 
@@ -139,7 +19,6 @@ ostream& print_board(ostream &o, int spacing, const vector<game_piece> & board, 
 	ostringstream board_disp;
 	board_disp << left;
 
-	//int length = board.size()
 	if (board.size() != dim_x * dim_y) {
 
 		cout << "BOARD MAINTAINS MORE PIECES THAN SPACE AVAILABLE" << endl;
