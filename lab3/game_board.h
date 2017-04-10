8422c02a293df938189ae76950a87260046f55aa
Jacob Schein <jacobschein0429@gmail.com>
2017-04-09 19:26:10 -0500

    Update game_board.h

--- lab3/game_board.h
+++ lab3/game_board.h
@@ -6,9 +6,6 @@
 #define GAMEBOARD_H
 
 
-int read_dimension(ifstream &, unsigned &, unsigned &);
-
-int read_board(ifstream &, vector<game_piece> &, unsigned, unsigned);
 
 ostream& print_board(ostream&, int, const vector<game_piece> &, unsigned int, unsigned int);
 
