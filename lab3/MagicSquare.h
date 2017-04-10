917e24b88ede883a7a362b70ab1fd0ee231bf152
Jacob Schein <jacobschein0429@gmail.com>
2017-04-09 19:25:42 -0500

    Update MagicSquare.h

--- lab3/MagicSquare.h
+++ lab3/MagicSquare.h
@@ -10,6 +10,9 @@
 
 const string MAGIC_EMPTY = " ";
 
+const int MAGIC_ROWS = 3;
+const int MAGIC_COLS = 3;
+
 class MagicSquareGame : public GameBase {
 public:
 	friend ostream& operator<< (ostream&, const MagicSquareGame&);
@@ -20,19 +23,11 @@ public:
 
 	virtual bool stalemate();
 	//helper method to tell whether a valid move exists between two points on the board
-	bool validMove(int, int, int, int);
 
 	virtual void prompt(unsigned &);
 
 	//turn method - executes at beginning of turn and grabs piece to move
 	virtual void turn();
-
-	//helper method to turn - grabs the location that piece wants to move to and checks if it is valid move
-	void turnHelper(unsigned, unsigned, string);
-
-	//helper method that handles transition between turns - ie. if user continues on same turn or switches to a different almond
-	void turnChangeHelper(unsigned, unsigned, string);
-
 	//play method as described in writeup
 
 	virtual void print() override;
